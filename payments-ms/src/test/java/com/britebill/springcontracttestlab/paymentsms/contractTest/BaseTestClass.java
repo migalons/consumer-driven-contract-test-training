cedced706b43f0866f8a34427c358a0c2ffdb308
Miguel A. Alonso <miguelal@amdocs.com>
2021-01-20 00:56:46 +0100

    Add sol1

--- payments-ms/src/test/java/com/britebill/springcontracttestlab/paymentsms/contractTest/BaseTestClass.java
+++ payments-ms/src/test/java/com/britebill/springcontracttestlab/paymentsms/contractTest/BaseTestClass.java
@@ -1,30 +1,27 @@
-//package com.britebill.springcontracttestlab.paymentsms.contractTest;
-//
-//import com.britebill.springcontracttestlab.paymentsms.PaymentsController;
-//import io.restassured.module.mockmvc.RestAssuredMockMvc;
-//import org.junit.Before;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
-//import org.springframework.test.annotation.DirtiesContext;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.time.LocalDateTime;
-//import java.time.format.DateTimeParseException;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-//@DirtiesContext
-//@AutoConfigureMessageVerifier
-//public class BaseTestClass {
-//
-//    @Autowired
-//    private PaymentsController paymentsController;
-//
-//    @Before
-//    public void setup() {
-//        RestAssuredMockMvc.standaloneSetup(paymentsController);
-//    }
-//
-//}
+package com.britebill.springcontracttestlab.paymentsms.contractTest;
+
+import com.britebill.springcontracttestlab.paymentsms.PaymentsController;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@DirtiesContext
+@AutoConfigureMessageVerifier
+public class BaseTestClass {
+
+    @Autowired
+    private PaymentsController paymentsController;
+
+    @Before
+    public void setup() {
+        RestAssuredMockMvc.standaloneSetup(paymentsController);
+    }
+
+}
--- payments-ms/src/test/resources/contracts/events-ms/shoudPostPayment.groovy
+++ payments-ms/src/test/resources/contracts/events-ms/shoudPostPayment.groovy
@@ -1,25 +1,25 @@
-//import org.springframework.cloud.contract.spec.Contract
-//
-//Contract.make {
-//    request {
-//        method(POST())
-//        url("/payments")
-//        headers {
-//            contentType(applicationJson())
-//        }
-//        body(
-//                holder: $(anyNonEmptyString()),
-//                number: $(regex('[0-9]{12,16}')),
-//                cvc: $(regex('[0-9]{3}')),
-//                expiration: $(regex('[0-9]{2}/[0-9]{2}')),
-//                amount: $(anyDouble())
-//        )
-//    }
-//
-//    response {
-//        status(CREATED())
-//        headers {
-//            header('location', anyUrl())
-//        }
-//    }
-//}
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method(POST())
+        url("/payments")
+        headers {
+            contentType(applicationJson())
+        }
+        body(
+                holder: $(anyNonEmptyString()),
+                number: $(regex('[0-9]{12,16}')),
+                cvc: $(regex('[0-9]{3}')),
+                expiration: $(regex('[0-9]{2}/[0-9]{2}')),
+                amount: $(anyDouble())
+        )
+    }
+
+    response {
+        status(CREATED())
+        headers {
+            header('location', anyUrl())
+        }
+    }
+}
